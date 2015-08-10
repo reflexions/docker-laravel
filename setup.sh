ab56eacb959c4b0476fb8ab1df160248a498f179
Patrick Way <patrick@reflexionsdata.com>
2015-08-10 11:51:18 -0400

    setup script

---
+++ setup.sh
@@ -0,0 +1,30 @@
+echo "-----------------------"
+echo "START setup.sh"
+echo "-----------------------"
+cd /var/www/application
+
+# configure composer
+composer config --global github-oauth.github.com $GITHUB_TOKEN
+composer config --global repo.packagist composer https://packagist.org
+composer install
+
+# configure apache
+cp vendor/reflexions/content-infrastructure/001-application.conf /etc/apache2/sites-available/
+ln -s /etc/apache2/sites-available/001-application.conf /etc/apache2/sites-enabled/
+unlink /etc/apache2/sites-enabled/000-default.conf
+    
+# crontab
+cp vendor/reflexions/content-infrastructure/crontab /var/spool/cron/crontabs/www-data
+chown www-data.crontab /var/spool/cron/crontabs/www-data
+chmod 0600 /var/spool/cron/crontabs/www-data
+
+# supervisor
+cp vendor/reflexions/content-infrastructure/supervisord.conf /etc/supervisor/
+cp vendor/reflexions/content-infrastructure/supervisor.conf.d/* /etc/supervisor/conf.d/
+mkdir /var/run/application/logs/supervisor/
+
+# flag that setup has run
+touch /var/run/application/setup-completed
+echo "-----------------------"
+echo "END setup.sh"
+echo "-----------------------"
